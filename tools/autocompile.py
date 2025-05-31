09391e070f1ec603de5e88f467cfcbc87eb6400b
Amar Persaud <tehspartaa@gmail.com>
2025-05-31 17:28:55 -0400

    Improve autocompile

--- tools/autocompile.py
+++ tools/autocompile.py
@@ -50,18 +50,30 @@ class MyHandler(FileSystemEventHandler):
                     with open(os.path.join(root, filename[:filename.rfind(".js")] + ".min.js"), 'w') as a:
                         a.write(d)
                     print("Succeeded in compressing: " + filename)
-                    
+    def CompressCSSFile(self, filename):
+        if(filename.endswith(".css") & (".min." not in filename)):
+            with open(filename, 'r') as g:
+                f = g.read()
+                d = compress(f)
+                with open(filename[:filename.rfind(".css")] + ".min.css", 'w') as a:
+                    a.write(d)     
+                    print("Succeeded in compressing: " + filename)
+    def CompressJSFile(self, filename):
+        if(filename.endswith(".js") & (".min." not in filename)):
+            with open(filename, 'r') as g:
+                f = g.read()
+                d = jsmin(f)
+                with open(filename[:filename.rfind(".js")] + ".min.js", 'w') as a:
+                    a.write(d)
+                    print("Succeeded in compressing: " + filename)
     #Parse HTML file and insert included HTML files
-    def ParseHTML(self, root, filename):
-        file_path = os.path.join(root, filename)
+    def ParseHTML(self, filename):
         html = "<html><body><include href=\"/nav.html\"/></body></html>"
         
         #open and read the html file
-        with open(file_path, 'rb') as f:
+        with open(filename, 'rb') as f:
             html = f.read()
-        
-        print("Opened file and read text")
-        
+            
         #make soup
         soup = BeautifulSoup(html, "html.parser")
         
@@ -78,14 +90,10 @@ class MyHandler(FileSystemEventHandler):
             
             #relative to the html file
             if(href.startswith(".")):
-                print("Relative to file")
-                includefilepath = os.path.join(root, href)
+                includefilepath = os.path.join(os.path.dirname(filename),href)
             else:   
                 #else relative to root / 
-                print("Relative to root")
-                includefilepath = os.path.join(href)
-            
-            print("\tInclude filepath: {}".format(includefilepath))
+                includefilepath = "./" + href
             
             replacementText = ""
             with open(includefilepath, 'rb') as incf:
@@ -95,10 +103,28 @@ class MyHandler(FileSystemEventHandler):
         
         return soup.prettify().encode('utf-8')
     
+    def CompileHTMLSRCFile(self, filename):
+        print("Found html file: {}".format(filename))
+        parsedHTML = self.ParseHTML(filename)
+        newFilepath = "." + filename.replace(HTML_SRC_LOCATION, "")
+        print("New filepath: {}".format(newFilepath))
+        with open(newFilepath, 'wb') as nf:
+            nf.write(parsedHTML) 
+    def CompileAllHTML(self):
+        for root, subdirs, files in os.walk(HTML_SRC_LOCATION):
+            if '.git' in subdirs:
+                subdirs.remove('.git')
+            for filename in files:
+                if(filename.endswith(".html")):
+                    print("Found html file: {} {}".format(root, filename))
+                    parsedHTML = self.ParseHTML(os.path.join(root,filename))
+                    newFilepath = os.path.join("./", root[len(HTML_SRC_LOCATION)+1:], filename)
+                    print("New filepath: {}".format(newFilepath))
+                    with open(newFilepath, 'wb') as nf:
+                        nf.write(parsedHTML)     
     def HandleEvents(self): # batch events
         # Deduplicate events
         files = []
-
         #Loop over queued filemodifiedevents and get the paths. Remove them from the queue.
         initialLength = len(self.EventsList) # use the current length to avoid getting stuck in loop if queue keeps expanding.
         for i in range(initialLength):
@@ -108,38 +134,38 @@ class MyHandler(FileSystemEventHandler):
                 files.append(event.src_path.replace("\\", "/"))
         handledFiles = 0
         
+        #If ANY include html file has changed, recompile all HTML Files.
+        #If just source files have changed, only recompile changed source files.
+        recompileAllHTML = False
+        for f in files:
+            if(f.startswith(HTML_INC_LOCATION) and f.endswith(".html")):
+                recompileAllHTML = True
+        if recompileAllHTML:
+            print("HTML INC file changed; recompiling all HTML files")
+            self.CompileAllHTML()  
+        
         #Handle the files.
         for f in files: 
             #Make sure the modified event is for the file and not the parent directory.
             if(os.path.isfile(f) and (".git" not in f)): # Also ensure not a git file
-                print(f"File is {f}")   
+                print(f"Change to file {f}")   
+                
                 #ignore python files. Case not necessary but nice to know
                 if(f.endswith(".py")):
                     print("python file, ignoring")
+                    
                 #Minify css and js files
-                if( (f.endswith(".css") and not f.endswith(".min.css")) or (f.endswith(".js") and not f.endswith(".min.js"))):
-                    print(f"css or js file changed, recompile: {f}")
-                    for d in MinifyDirectories:
-                        self.CheckDirectory(d)
+                if(f.endswith(".css") and (not f.endswith(".min.css"))):
+                    self.CompressCSSFile(f)
+                    
+                if(f.endswith(".js") and (not f.endswith(".min.js"))):
+                    self.CompressJSFile(f)
+                    
                 #Compile html files if they are part of the inc or 
-                if(f.endswith(".html")):
-                    #Only recompile if source or included html has changed.
-                    if(f.startswith(HTML_SRC_LOCATION) or f.startswith(HTML_INC_LOCATION)):
-                        for root, subdirs, files in os.walk(HTML_SRC_LOCATION):
-                            if '.git' in subdirs:
-                                subdirs.remove('.git')
-                            for filename in files:
-                                if(filename.endswith(".html")):
-                                    print("Found html file: {} {}".format(root, filename))
-                                    parsedHTML = self.ParseHTML(root, filename)
-                                    newFilepath = os.path.join("./", root[len(HTML_SRC_LOCATION)+1:], filename)
-                                    print("New filepath: {}".format(newFilepath))
-                                    with open(newFilepath, 'wb') as nf:
-                                        nf.write(parsedHTML)
-                    else:
-                        print("Not SRC or INC file. Ignoring")
-                handledFiles = handledFiles + 1
-        print(f"Handled {handledFiles} events at t={datetime.now()}")
+                if(f.endswith(".html") and f.startswith(HTML_SRC_LOCATION) and not recompileAllHTML):
+                    self.CompileHTMLSRCFile(f)
+ 
+        print(f"Handled {len(files)} events at t={datetime.now()}")
 
 if __name__ == "__main__":
     event_handler = MyHandler()
